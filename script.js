1c29d0b910fd91b7e57f34a9e66fede4bcbd843f
Littlelinkrocks <77841378+Littlelinkrocks@users.noreply.github.com>
2021-01-31 13:28:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-<h2>My first webpage</h2>
-<h2>Justin liverpool</h2>
-<main>
-  <p>Click here to view more <a href="#">cat photos</a>.</p>
+    var firstName = "Justin";
+    var lastName = "Liverpool";
+    var fullName = firstName + " " + lastName;
+    var age = "14";
+    System.out.println("my name is" + fullName);
+    System.out.println("my age is" + age);
 
-  <a href="#"><img src="https://www.instagram.com/p/B5jJI06pPB5QTwlq83G1h4eUHDgYYSHGR6vbSo0/" alt="Picture of me."></a>
-
-  <p>One thing I did over my break was try to catch up to this show called one piece. The show has 958 epsiodes and still going. I am currently on episode 387 and have been watching it for about a week now. I also have been playing this game called persona 5 im about 26 hours into the game and im having tons of fun.</p>
-  <p>Things I like to do:</p>
-  <ul>
-    <li>Anime</li>
-    <li>Games</li>
-    <li>Sports</li>
-  </ul>
- 
-
-</main>
+    console.log("Im having tons of fun in the class");

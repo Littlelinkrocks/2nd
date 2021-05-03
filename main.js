d5815cca21c7fb61d4beca5789e1caf639235892
Littlelinkrocks <77841378+Littlelinkrocks@users.noreply.github.com>
2021-05-02 20:52:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
   var firstName = "Justin";
+
     var lastName = "Liverpool";
+
     var fullName = firstName + " " + lastName;
+
     var age = "14";
+
     alert("my name is" + fullName);
+
     alert("my age is" + age);
 
     console.log("Im having tons of fun in the class");
